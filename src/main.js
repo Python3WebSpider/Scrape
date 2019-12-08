aaaabf08a2b02a9a5525bd0baf58f32ca8b1a8a8
Germey <cqc@cuiqingcai.com>
2019-12-09 02:04:55 +0800

    init hook

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import store from './store.js'
+
+Vue.use(VueAxios, axios)
+
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Step1.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row>
+    <el-col :span="8" :offset="8">
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="Username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="Password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">Submit</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import store from '../store'
+
+  export default {
+    name: "Step1",
+    data() {
+      return {
+        form: {
+          username: null,
+          password: null,
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        let token = JSON.stringify(this.form)
+        this.$http.post(store.state.url.root, {
+          token: token
+        }).then((data) => {
+          console.log('data', data)
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
